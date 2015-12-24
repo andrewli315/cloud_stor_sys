a816f67f627e97fef468ba83fc9635c441af9e87
andrewli <nyps70414@gmail.com>
2015-12-24 20:52:46 +0800

    finally transmit the message

--- client/Client.java
+++ client/Client.java
@@ -1,7 +1,9 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.PrintStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentSkipListSet;
@@ -13,34 +15,40 @@ import ezprivacy.toolkit.CipherUtil;
 import ezprivacy.secret.EnhancedProfileManager;
 import ezprivacy.service.authsocket.AuthSocketServer;
 import ezprivacy.service.acs.ACS;
-import ezprivacy.service.authsocket.EnhancedAuthSocketServerAcceptor;
+import ezprivacy.service.authsocket.EnhancedAuthSocketClient;
 import ezprivacy.service.register.EnhancedProfileRegistrationClient;
 
 
 class Client{
-	EnhancedAuthSocketClient client;
 	DataInputStream netIn;
 	DataOutputStream netOut;
-	BufferedReader reader;
+	BufferedReader input;
+	BufferedOutputStream out;
 	String IP;
 	int PORT;
 	File card = new File("client.card");//註冊產生的卡
-	String password = "0934203764";//註冊產生的卡與相對應的密碼
+	String password = "1234";//註冊產生的卡與相對應的密碼
 	EnhancedProfileManager profile;
 	EnhancedAuthSocketClient client;
 	
 	
 	
 	//constructor of Client class, in order to create needy variable and GUI　surface
-	public Client(){
+	public Client()throws Exception{
+		input = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("input the ip and port: ...");
+		IP=input.readLine();
+		PORT =3038; 
+		connect();
+		String msg = input.readLine();
+		trans_MSG(msg);
+		close();
+		return;
 		
 	}
 	//connect to the server
-	public void connect(int port,String ip){
+	public void connect()throws Exception{
 		System.out.println("===== start EnhancedAuthSocketClientTest =====");
-			
-			
-			
 			
 			//根據client的卡和密碼載入client的profile
 			profile = EZCardLoader.loadEnhancedProfile(card, password);
@@ -48,7 +56,7 @@ class Client{
 			
 			//產生一個client物件
 			client = new EnhancedAuthSocketClient(profile);
-			client.connect("localhost", 1235);//連接到server與對應的port
+			client.connect(IP, PORT);//連接到server與對應的port
             
 			//------------- ACS為server,clent之間做MAKD金鑰分配，此時client會與server分享一把sessionkey------------------------//
 			client.doEnhancedKeyDistribution();
@@ -66,15 +74,36 @@ class Client{
 		
 	}
 	//transmit the message, include the CMD, File name,and something else;
-	public void trans_MSG(){
+	public void trans_MSG(String msg)throws Exception{
+		
+		netOut = new DataOutputStream(client.getOutputStream());
+		//SK事實上為256位元長度，我們將其拆對半分別作為加密金鑰與IV//
+		//--------------加解密前先把key和iv拿出---------------------------------------//
+		byte[] sk = client.getSessionKey().getKeyValue();
+		byte[] k = CipherUtil.copy(sk, 0, CipherUtil.KEY_LENGTH);
+		byte[] iv = CipherUtil.copy(sk, CipherUtil.KEY_LENGTH, CipherUtil.BLOCK_LENGTH);
+		//--------------加解密前先把key和iv拿出---------------------------------------//
+			
+		//--------------加密欲傳送的內容--------------------------------------------------//
+		byte[] encrypted_msg = CipherUtil.authEncrypt(k, iv, msg.getBytes());
+		//--------------加密欲傳送的內容--------------------------------------------------//
+            
+		//--------------輸出密文-------------------------------------------------------//
+		netOut.writeInt(encrypted_msg.length);
+		netOut.write(encrypted_msg);
+		netOut.flush();
+		
+		//--------------輸出密文-------------------------------------------------------//
+		
+		
 		
 	}
 	//create a new FTPClient class(the class define in another java file, mainly dealing with the data transmission) to create a new data channel
-	public trans_File(String f){
+	public void trans_File(String f){
 		
 	}
 	//the purpose is also to create a new data channel to transmit the file
-	public recieve_File(String f){
+	public void recieve_File(String f){
 		
 	}
 	//recieve the Signature from the Server and save as a file;
@@ -86,7 +115,8 @@ class Client{
 		
 	}
 	//close the client socket
-	public void close(){
-		
+	public void close()throws Exception{
+		netOut.close();
+		client.close();
 	}
 }
